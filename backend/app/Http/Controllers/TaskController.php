4db9aec5828fcf8b89db01cd7ef471123b4beb47
shaker sajib <shakertech18@gmail.com>
2026-04-27 14:44:35 +0600

    2nd push

--- backend/app/Http/Controllers/TaskController.php
+++ backend/app/Http/Controllers/TaskController.php
@@ -64,6 +64,10 @@ class TaskController extends Controller
 
         $data = $validated;
 
+        if (empty($data['priority'])) {
+            $data['priority'] = 'medium';
+        }
+
         if ($user instanceof User) {
             $data['user_id'] = $user->id;
         } elseif ($user instanceof License) {
@@ -229,6 +233,7 @@ class TaskController extends Controller
 
         $tasks = Task::where('paylink','=',null)
         ->where('status','!=','invalid')
+        ->where('user_id','=',null)
         ->orderByRaw("CASE priority WHEN 'high' THEN 1 WHEN 'medium' THEN 2 WHEN 'low' THEN 3 ELSE 4 END")
         ->offset($offset)
         ->limit($limit)

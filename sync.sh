54486b7a28f57a2797b35e52effac929aa12d33a
shaker sajib <shakertech18@gmail.com>
2026-04-29 23:37:07 +0600

    push

--- sync.sh
+++ sync.sh
@@ -10,3 +10,6 @@ echo "Resetting local branch to origin/main..."
 git reset --hard origin/main
 
 echo "Done. Your branch is now synced with origin/main."
+
+sudo chmod -R 777 backend/storage
+sudo chmod -R 777 backend/bootstrap/cache
